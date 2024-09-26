e7d738880b3bb1be0823e32d09adbfc439eec622
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-09-26 23:04:10 +0500

    working on create video module

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,11 +1,29 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { ScrollView, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import FormField from "../../components/FormField";
 
 const Create = () => {
+  const [uploading, setUploading] = useState(false);
+  const [form, setForm] = useState({
+    title: "",
+    video: "",
+    thumbnail: "",
+    prompt: "",
+  });
   return (
-    <View>
-      <Text>Create</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full ">
+      <ScrollView className="px-4 my-6">
+        <Text className="text-2xl text-white font-semibold">Upload Video</Text>
+        <FormField
+          title="Video"
+          value={form.title}
+          placeholder="Give your video a catchy title"
+          otherStyles={"mt-10"}
+          handleChangeText={(text) => setForm({ ...form, title: text })}
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
